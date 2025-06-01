f66f4f0fda08e77398025c320bba716016f37801
Lucas Rodrigues Severo <lucasrodriguesevero@gmail.com>
2025-05-31 20:49:58 -0400

    Adicionado pagina

--- script.js
+++ script.js
@@ -6,29 +6,24 @@ const sheetId = "1oLSDoPsxTmbXAaohCnrc14ngVLursBJoEpezFsk9s9k"
 const sheetName = "referencia"
 const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=${sheetName}`;
 
-fetch(sheetURL)
-  .then((response) => {
-    console.log(response)
-    const valor =  response.text()
-    console.log(valor)
-    return valor
-})
-.then((csvText) => {
-  const valor = handleResponse(csvText)
-  console.log(valor)
-  return valor
-});
+const resposta = await fetch(sheetURL)
+const csvText = await resposta.text()
+const valores = await handleResponse(csvText)
 
-function handleResponse(csvText) {
+async function handleResponse(csvText) {
   console.log(csvText)
-  let sheetObjects = csvToObjects(csvText);
+  let sheetObjects = await csvToObjects(csvText);
   // sheetObjects is now an Array of Objects
   console.log(sheetObjects);
   // ADD CODE HERE
 }
 
-function csvToObjects(csv) {
+async function csvToObjects(csv) {
   const csvRows = csv.split("\n");
+
+  console.log("Valores quebrados \n\n")
+  console.log(csvRows)
+
   const propertyNames = csvSplit(csvRows[0]);
   let objects = [];
   for (let i = 1, max = csvRows.length; i < max; i++) {
@@ -36,12 +31,6 @@ function csvToObjects(csv) {
     let row = csvSplit(csvRows[i]);
     for (let j = 0, max = row.length; j < max; j++) {
       thisObject[propertyNames[j]] = row[j];
-      // BELOW 4 LINES WILL CONVERT DATES IN THE "ENROLLED" COLUMN TO JS DATE OBJECTS
-      // if (propertyNames[j] === "Enrolled") {
-      //   thisObject[propertyNames[j]] = new Date(row[j]);
-      // } else {
-      //   thisObject[propertyNames[j]] = row[j];
-      // }
     }
     objects.push(thisObject);
   }

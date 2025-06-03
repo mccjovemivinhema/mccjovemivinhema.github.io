67b8a2f51ec35cd04b46f52945bcd22fd5ce0e5e
Lucas Rodrigues Severo <lucasrodriguesevero@gmail.com>
2025-06-03 13:41:40 -0400

    Alteração de página dos dados dos cursilhistas

--- cursilhistas/detalhes/script.js
+++ cursilhistas/detalhes/script.js
@@ -8,12 +8,28 @@ const nomeValor = document.getElementById("nomeValor")
 const dataAniversarioValor = document.getElementById("dataAniversarioValor")
 const cursilhoValor = document.getElementById("cursilhoValor")
 const padrinhoMadrinhaValor = document.getElementById("padrinhoMadrinhaValor")
-const indicacoes = document.getElementById("indicacoes")
+const quantidadeIndicacoes = document.getElementById("quantidadeIndicacoes")
+const indicacoes = document.getElementById("listaIndicacoes")
+
+const listaIndicacoes = item.indicacoes.split(";")
+
+if (listaIndicacoes.length > 0 && listaIndicacoes[0] !== "Nenhuma indicação") {
+    quantidadeIndicacoes.textContent = `${listaIndicacoes.length} indicações`
+}
 
 nomeValor.textContent = item.nome
 dataAniversarioValor.textContent = item.dataAniversario
 cursilhoValor.textContent = item.cursilho
-indicacoes.textContent = item.indicacoes
+
+for (let indicacao of listaIndicacoes) {
+    const tr = document.createElement("tr")
+    const td = document.createElement("td")
+
+    td.textContent = indicacao
+
+    tr.appendChild(td)
+    indicacoes.appendChild(tr)
+}
 
 console.log(item)
 
